3a95acc6edf7135f4d0aa0fb4608ab30995fad07
raditya putranto <radityaputranto97@yahoo.co.id>
2021-01-06 22:08:53 +0700

    13 validate route params

--- nuxtjs/tutorial-nuxt/components/Navbar.vue
+++ nuxtjs/tutorial-nuxt/components/Navbar.vue
@@ -11,10 +11,11 @@
               <b-nav-item to="/about">About</b-nav-item>
               <b-nav-item to="/contact">Contact</b-nav-item>
               <b-nav-item to="/blog">Blog</b-nav-item>
-              <b-nav-item-dropdown text="User" right>
+              <b-nav-item to="/user">User</b-nav-item>
+              <!-- <b-nav-item-dropdown text="User" right>
                 <b-dropdown-item to="/user">User List</b-dropdown-item>
                 <b-dropdown-item to="/user/profile">Profile</b-dropdown-item>
-              </b-nav-item-dropdown>
+              </b-nav-item-dropdown> -->
             </b-navbar-nav>
           </b-collapse>
         </b-navbar>
--- nuxtjs/tutorial-nuxt/pages/user/_id.vue
+++ nuxtjs/tutorial-nuxt/pages/user/_id.vue
@@ -3,3 +3,11 @@
         <h1>Profile {{$route.params.id}}</h1>
     </div>
 </template>
+
+<script>
+export default{
+    validate({ params }){
+        return /^\d+$/.test(params.id)
+    }
+}
+</script>
