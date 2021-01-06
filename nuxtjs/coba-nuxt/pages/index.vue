2321a60df851468639654e18691599302fcfb7d2
raditya putranto <radityaputranto97@yahoo.co.id>
2021-01-06 16:41:06 +0700

    1-5 overview

--- nuxtjs/coba-nuxt/pages/index.vue
+++ nuxtjs/coba-nuxt/pages/index.vue
@@ -1,3 +1,16 @@
 <template>
-    <h1>Hello Nuxt</h1>
+    <div id="app">
+        <nuxt-link to="/about">About</nuxt-link> | 
+        <nuxt-link to="/contact">Contact</nuxt-link>
+        <h1>Hello Nuxt!</h1>
+    </div>
 </template>
+
+<script></script>
+
+<style>
+    body{
+        font-family: sans-serif;
+        margin: 5%;
+    }
+</style>
