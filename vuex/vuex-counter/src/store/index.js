56b45570fb6f2bb8078cca625aa4f9d724639d90
raditya putranto <radityaputranto97@yahoo.co.id>
2021-01-16 22:13:56 +0700

    vuex - counter

---
+++ vuex/vuex-counter/src/store/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+	state: {
+		count: 0
+	},
+	getters: {
+		getCounter: state => {
+			return state.count
+		}
+	},
+	mutations: {
+		increment (state) {
+			state.count++	
+		},
+		decrement (state) {
+			if(state.count > 0) {
+				state.count--
+			}
+		}
+	}	
+})
