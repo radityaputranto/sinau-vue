f1bbbba05ae0a2caa8c139bff1e37515eac80001
raditya putranto <radityaputranto97@yahoo.co.id>
2021-06-17 16:23:49 +0700

    8 transitioning between elements

--- vue_animation/src/components/Todos.vue
+++ vue_animation/src/components/Todos.vue
@@ -6,14 +6,16 @@
       @keypress.enter="addTodo"
       placeholder="Add a new todo..."
     />
-    <div v-if="todos.length">
-      <transition-group tag="ul" name="list" appear>
-        <li v-for="todo in todos" :key="todo.id" @click="deleteTodo(todo.id)">
-          {{ todo.text }}
-        </li>
-      </transition-group>
-    </div>
-    <div v-else>Woohoo, nothing left todo!</div>
+    <transition name="switch" mode="out-in">
+      <div v-if="todos.length">
+        <transition-group tag="ul" name="list" appear>
+          <li v-for="todo in todos" :key="todo.id" @click="deleteTodo(todo.id)">
+            {{ todo.text }}
+          </li>
+        </transition-group>
+      </div>
+      <div v-else>Woohoo, nothing left todo!</div>
+    </transition>
   </div>
 </template>
 
@@ -107,5 +109,22 @@ input {
 
 .list-move {
   transition: all 0.3 ease;
+}
+
+.switch-enter-from,
+.switch-leave-to {
+  opacity: 0;
+  transform: translateY(20px);
+}
+/* commented because it is default properties */
+/* .switch-enter-to,
+.switch-leave-from {
+  opacity: 1;
+  transform: translateY(0px);
+} */
+
+.switch-enter-active,
+.switch-leave-active {
+  transition: all 0.5s ease;
 }
 </style>
