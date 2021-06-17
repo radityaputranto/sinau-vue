4343e1674b03dce76db9eeb961e8b7e26cceb4f5
raditya putranto <radityaputranto97@yahoo.co.id>
2021-06-17 15:59:14 +0700

    5 adding css keyframes

--- vue_animation/src/views/Home.vue
+++ vue_animation/src/views/Home.vue
@@ -56,16 +56,17 @@ export default {
 }
 
 /* toast animation */
-.toast-enter-from {
+/* .toast-enter-from {
   opacity: 0;
   transform: translateY(-60px);
 }
 .toast-enter-to {
   opacity: 1;
   transform: translateY(0);
-}
+} */
 .toast-enter-active {
-  transition: all 0.3s ease;
+  /* transition: all 0.3s ease; */
+  animation: wobble 0.5s ease;
 }
 .toast-leave-from {
   opacity: 1;
@@ -77,5 +78,31 @@ export default {
 }
 .toast-leave-active {
   transition: all 0.3s ease;
+}
+
+@keyframes wobble {
+  0% {
+    transform: translateY(-60px);
+    opacity: 0;
+  }
+  50% {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+  60% {
+    transform: translateX(8px);
+  }
+  70% {
+    transform: translateX(-8px);
+  }
+  80% {
+    transform: translateX(4px);
+  }
+  90% {
+    transform: translateX(-4px);
+  }
+  100% {
+    transform: translateX(0px);
+  }
 }
 </style>
