26dd62edf8ab0cb315a44eadd578362d027bd7ed
raditya putranto <radityaputranto97@yahoo.co.id>
2021-06-17 18:24:01 +0700

    12 router transition

--- vue_animation/src/App.vue
+++ vue_animation/src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> | 
+    <router-link to="/about">About</router-link> |
     <router-link to="/contact">Contact</router-link>
   </div>
-  <router-view/>
+
+  <router-view v-slot="{ Component }">
+    <transition name="route" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
 </template>
+
+
 
 <style>
 body {
@@ -24,5 +31,21 @@ body {
 }
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+/* transition */
+.route-enter-from {
+  opacity: 0;
+  transform: translateX(100px);
+}
+.route-enter-active {
+  transition: all 0.3s eas-out;
+}
+.route-leave-to {
+  opacity: 0;
+  transform: translateX(-100px);
+}
+.route-leave-active {
+  transition: all 0.3s ease-in;
 }
 </style>
